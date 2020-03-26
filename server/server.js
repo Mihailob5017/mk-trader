7ea763a03249c35dcfb11b8bb3559bf3a040e178
Mihailob5017 <mihailob5017@gmail.com>
2020-03-26 17:25:35 +0100

    install all the requierd libraries

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log('server started on port ' + PORT);
+});
