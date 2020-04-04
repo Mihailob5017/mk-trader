bbf7460c31620c8a2ee7737b6181ba9656939262
Mihailob5017 <mihailob5017@gmail.com>
2020-04-04 23:26:13 +0200

    added the start page overview

--- client/src/components/page-overview/page-overview.component.js
+++ client/src/components/page-overview/page-overview.component.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './page-overview.style.scss';
-const PageOverview = ({ name, link }) => {
+const PageOverview = ({ children, to }) => {
   return (
     <div className="page-overview-container">
       <h2 className="page-overview-text">
-        <Link to={link}>{name}</Link>
+        <Link to={to}>{children}</Link>
       </h2>
     </div>
   );
--- client/src/pages/start/start.page.js
+++ client/src/pages/start/start.page.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import './start-page.style.scss';
-
+import PageOverview from '../../components/page-overview/page-overview.component';
 const startPage = () => {
-  return <div className="start-page-container"></div>;
+  return <div className="start-page-container">
+    <PageOverview to='/home'>Home </PageOverview>
+    <PageOverview to='/profile'>Profile</PageOverview>
+    <PageOverview to='/add'>Add</PageOverview>
+    <PageOverview to='/sign'>Sign </PageOverview>
+  </div>;
 };
 
 export default startPage;
