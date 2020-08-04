493a88bb5492aa4580ca86f6182c42e48f9ec0a3
Mihailob5017 <mihailob@gmail.com>
2020-08-04 14:51:46 +0200

    built the project

---
+++ client/build/precache-manifest.d5850f4fc87fab7a5cb477aa36898854.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44511e749bbd8e2f809f99aac6a2c195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c62dbc83b7d9544cc2c",
+    "url": "/static/css/main.878104d3.chunk.css"
+  },
+  {
+    "revision": "224579405e5a426ae9d2",
+    "url": "/static/js/2.f2c95085.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2c95085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c62dbc83b7d9544cc2c",
+    "url": "/static/js/main.c4458202.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "c0402c643e16632c389cd491f5888383",
+    "url": "/static/media/add.c0402c64.jpeg"
+  },
+  {
+    "revision": "478bac88eb5cdcb3bc842b83ea50a1fe",
+    "url": "/static/media/default_f.478bac88.png"
+  },
+  {
+    "revision": "04108d917567052ffad4bc9e82257372",
+    "url": "/static/media/default_m.04108d91.png"
+  },
+  {
+    "revision": "a658e64ff91f9e1bbda93e882ec1f44d",
+    "url": "/static/media/female.a658e64f.png"
+  },
+  {
+    "revision": "dc2941f2b067dcb9d7044c620f1a643f",
+    "url": "/static/media/gold_f.dc2941f2.png"
+  },
+  {
+    "revision": "5f3e5664518c04a4dac381f303b8672b",
+    "url": "/static/media/gold_m.5f3e5664.png"
+  },
+  {
+    "revision": "678d22f54ce98e049b3f65e1dd4f95dc",
+    "url": "/static/media/hats.678d22f5.png"
+  },
+  {
+    "revision": "b8b3ce169de01c2d8cb3abae8991b82c",
+    "url": "/static/media/heels.b8b3ce16.png"
+  },
+  {
+    "revision": "82b08cd3803cd3423e3e22356091673c",
+    "url": "/static/media/homepage.82b08cd3.jpeg"
+  },
+  {
+    "revision": "508aeda03318e8e8948ee2d2b61480b1",
+    "url": "/static/media/hoodies.508aeda0.png"
+  },
+  {
+    "revision": "b1d26bb638b28b568e23127b35c885af",
+    "url": "/static/media/jackets.b1d26bb6.png"
+  },
+  {
+    "revision": "e5c54a7b7b033f2038ef794c9b390b6c",
+    "url": "/static/media/male.e5c54a7b.png"
+  },
+  {
+    "revision": "b3a95bb8931dc1384de34c4375ce73fa",
+    "url": "/static/media/ocasional_f.b3a95bb8.png"
+  },
+  {
+    "revision": "dd5ca2aff7413343bc93aced5bd35d85",
+    "url": "/static/media/ocasional_m.dd5ca2af.png"
+  },
+  {
+    "revision": "be0200e65e8588cd2450d63aeb2a00a6",
+    "url": "/static/media/other.be0200e6.png"
+  },
+  {
+    "revision": "a1c81e021fbce40ee2b12e8f1e093939",
+    "url": "/static/media/pants.a1c81e02.png"
+  },
+  {
+    "revision": "266a7831ffe4037ad774f06e894a9141",
+    "url": "/static/media/polos.266a7831.png"
+  },
+  {
+    "revision": "c415698be5b51c8f43d1b614b69b8fa0",
+    "url": "/static/media/premium_f.c415698b.png"
+  },
+  {
+    "revision": "0423536c4b0bdd3eb7c0e7d3f29a11cd",
+    "url": "/static/media/premium_m.0423536c.png"
+  },
+  {
+    "revision": "6bc636a645dcd700dbd4852f86061350",
+    "url": "/static/media/proffesional_f.6bc636a6.png"
+  },
+  {
+    "revision": "2a09c1930b3a666b71421e8c0677afa7",
+    "url": "/static/media/proffesional_m.2a09c193.png"
+  },
+  {
+    "revision": "8cbda2f98bcc67c1f84790953c4560c5",
+    "url": "/static/media/profile.8cbda2f9.jpg"
+  },
+  {
+    "revision": "e05aa35caf5d95a7acf11e5825cb0c42",
+    "url": "/static/media/shirts.e05aa35c.png"
+  },
+  {
+    "revision": "9486dad1b02d629edb23852dce936766",
+    "url": "/static/media/sign.9486dad1.jpeg"
+  },
+  {
+    "revision": "f854fc200b9c7fe32bb532c7c4dcb135",
+    "url": "/static/media/sneakers.f854fc20.png"
+  },
+  {
+    "revision": "feaca28d1e417208975a6647822fd0a2",
+    "url": "/static/media/socks.feaca28d.png"
+  },
+  {
+    "revision": "fcd94ae6faa0496a81f9da12b70dcf46",
+    "url": "/static/media/sweaters.fcd94ae6.png"
+  },
+  {
+    "revision": "ac370c5b598ce7215ca5300d86f205c1",
+    "url": "/static/media/swimsuits.ac370c5b.png"
+  }
+]);
